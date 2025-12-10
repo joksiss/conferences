f71e5da5da03a8e9c8c94cdb01bfdf9847ffdbf1
jokubelis <jokubasmoc@gmail.com>
2025-12-10 21:39:27 +0200

    added translation file

---
+++ resources/lang/lt/app.php
@@ -0,0 +1,41 @@
+<?php
+
+return [
+
+    // Bendri
+    'conferences' => 'Konferencijos',
+    'conference' => 'Konferencija',
+    'actions' => 'Veiksmai',
+    'view' => 'Peržiūrėti',
+    'edit' => 'Redaguoti',
+    'delete' => 'Ištrinti',
+    'login' => 'Prisijungti',
+    'logout' => 'Atsijungti',
+    'register' => 'Registruotis',
+    'back' => 'Atgal',
+    'hello' => 'Sveiki',
+
+    // Konferencijų laukai
+    'title' => 'Pavadinimas',
+    'description' => 'Aprašymas',
+    'conference_date' => 'Konferencijos data',
+    'date' => 'Data',
+    'address' => 'Adresas',
+    'participants' => 'Dalyvių skaičius',
+    'city' => 'Miestas',
+
+    // CRUD tekstai
+    'add' => 'Pridėti',
+    'update' => 'Išsaugoti',
+    'add_conference' => 'Pridėti konferenciją',
+    'create_conference' => 'Nauja konferencija',
+    'edit_conference' => 'Redaguoti konferenciją',
+
+    // Sėkmės pranešimai
+    'conference_created' => 'Konferencija sėkmingai sukurta!',
+    'conference_updated' => 'Konferencija sėkmingai atnaujinta!',
+    'conference_deleted' => 'Konferencija sėkmingai ištrinta!',
+
+    // Patvirtinimai
+    'confirm_delete' => 'Ar tikrai norite ištrinti šią konferenciją?',
+];
